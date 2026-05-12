3fa5d912835d8f0a0c1aace041c250a5846e52f0
kim7638 <mkt@mwkorea.co.kr>
2026-05-13 08:59:38 +0900

    fix: GitHub Pages 경로 맞게 start_url, scope, ASSETS 수정

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'mic-pwa-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const ASSETS = ['/karaoke-web/', '/karaoke-web/index.html', '/karaoke-web/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
